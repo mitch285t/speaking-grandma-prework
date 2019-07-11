c96b377e4f6386cfd70cc007f694bd149fbbbab4
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-11 05:40:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@ def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
+  else phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
 end 
 end
